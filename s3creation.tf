f60e91d8d52c94d03adf69ddc881d1e0d609c735
OmkarSahastrabudhe <omkarsahastrabudheofficial@gmail.com>
2026-02-18 15:10:25 +0530

    commit it

---
+++ s3creation.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+resource "aws_s3_bucket" "my-bucket" {
+    bucket = "my-working-bucket"
+
+    
+    
+  
+}
+resource "aws_s3_bucket_acl" "exposingBucketAcl" {
+    bucket = aws_s3_bucket.my-bucket.bucket
+    acl = "public-read"
+}
+
+resource "aws_s3_bucket_object" "mys3Obj" {
+  for_each = fileset("/home/ubuntu/static-website", "**")
+  bucket = aws_s3_bucket.my-bucket.bucket
+  key = each.value
+   source = "/home/ubuntu/static-website/${each.value}"
+  acl    = "public-read"
+
+
+  
+}
+
+resource "aws_s3_bucket_website_configuration" "websiteconfig" {
+bucket = aws_s3_bucket.my-bucket.bucket
+index_document {
+  suffix = "index.html"
+}
+
+error_document {
+  key = "error.html"
+}
+
+  
+}
+output "website_url"{
+    value = aws_s3_bucket_website_configuration.websiteconfig.website_endpoint
+}
+
